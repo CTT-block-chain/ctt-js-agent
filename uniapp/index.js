f878f4d4b8fe4d927f9233b666afff99c851b8d1
pfcoder <pfcoder97@gmail.com>
2020-04-26 16:07:10 +0800

    change to waiting wasm ready for keyring

--- uniapp/index.js
+++ uniapp/index.js
@@ -1,6 +1,7 @@
 const Sub = require("../lib/sub");
 
-window.subInit = (node) => Sub.init(node);
+window.subInitKeyring = () => Sub.initKeyring();
+window.subInitApi = (node) => Sub.initApi(node);
 window.subNewAccount = (name, pwd) => Sub.newAccount(name, pwd);
 window.subSetupAccountByJson = (json) => Sub.setupAccountByJson(json);
 window.subUnlock = (address, pwd) => Sub.unlock(address, pwd);
